28a40626e0c30e789e56777fa265eb8cc661e746
jcmjcm10 <u1973231@campus.udg.edu>
2022-06-20 23:24:01 +0200

    flexas desparaecen

--- scripts/scr_arrow.gd
+++ scripts/scr_arrow.gd
@@ -10,6 +10,7 @@ var dir = Vector2.RIGHT
 
 func _ready():
 	rotation = dir.angle()
+	$Death.start()
 	pass 
 
 func _process(delta):
@@ -24,4 +25,9 @@ func _on_Area2D_body_entered(body):
 	if body.is_in_group("enemy"):
 		body.recivirDamage(damage,position)
 		queue_free()
+	pass # Replace with function body.
+
+
+func _on_Death_timeout():
+	queue_free()
 	pass # Replace with function body.
